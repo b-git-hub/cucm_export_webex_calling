9e0ba495bd477d871de7d6497e06c40cf3f4e9cf
Brian <bmcb555@gmail.com>
2020-11-18 21:14:35 +0000

    Completed - Required 2 lookups and a ZIP

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ from requests.auth import HTTPBasicAuth
 from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning
 from lxml import etree
+import csv
  
  
 disable_warnings(InsecureRequestWarning)
@@ -40,32 +41,56 @@ service = client.create_service(binding, location)
 def show_history():
     for item in [history.last_sent, history.last_received]:
         print(etree.tostring(item["envelope"], encoding="unicode", pretty_print=True))
-'''
-try:
-    resp = service.listPhone(searchCriteria={'name': 'd%'}, 
-                             returnedTags={'name': '', 'description': ''})
-    print(resp)
-except Fault:
-    show_history()
 
-try:
-    resp = service.listLine(searchCriteria={'pattern': '%'},
-                             returnedTags={'pattern': ''})
-    print(resp)
-except Fault:
-    show_history()
-'''
+
+user_subscription=int(input("Enter the user subscribtion number: "))
+user_subscription = f'Sub{user_subscription}'
+webex_enterprise = input('Enter webex domain URL: ')
+location_user = input('Enter the user location (manually modify if multiple locations): ')
+
+print(user_subscription)
+
 try:
     resp1 = service.listUser(searchCriteria={'userid': '%'}, 
                              returnedTags={'userid': '', 'firstName': '', 'lastName': '', 'mailid': '', 'primaryExtension': ''})
-    print(resp1)
+    resp1 = resp1['return']['user']
 except Fault:
     show_history()
 
 try:
     resp2 = service.listUser(searchCriteria={'userid': '%'}, 
                              returnedTags={'userid': '', 'firstName': '', 'lastName': '', 'telephoneNumber': ''})
-    print(resp2)
+    resp2 = resp2['return']['user']
 except Fault:
     show_history()
 
+
+with open('webex_calling_user.csv', 'w', newline='' ) as f:
+    fieldnames = [
+        'First Name', 'Last Name', 'Display Name', 'UserID/Email (Required)','User Status',
+        'Last Service Accessed Time', 'Days since Last Service Accessed', 'Extension',
+        'Phone Number', 'Caller ID Number', 'Caller ID First Name', 'Caller ID Last Name','Location', 'Hybrid Calendar Service Resource Group',
+        'Hybrid Call Service Resource Group', 'Jabber with Webex Teams', 'Jabber Calling', 'UC Manager Profile',
+        'Contact Migration Required', 'Upgrade Profile', 'Calling Behavior', 'Calling Behavior UC Manager Profile',
+        'Call Service Aware', 'Call Service Connect','Enterprise Content Management', 'Hybrid Calendar Service (Exchange)',
+        'Hybrid Calendar Service (Google)', 'Hybrid Message Service', 'Webex Meeting Assistant', f'Care Digital Channel [{user_subscription}]',
+        f'Webex Team Meeting [{user_subscription}]', f'{webex_enterprise} - WebEx Enterprise Edition [{user_subscription}]', 
+        f'Webex Calling SP Enterprise [{user_subscription}]', f'Webex Teams [{user_subscription}]'
+        ]
+    thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+    thewriter.writeheader()
+    for i,b in zip(resp1, resp2):
+        thewriter.writerow({
+            'First Name' : i['firstName'], 'Last Name' : i['lastName'],
+            'Display Name' : i['firstName'] + ' ' + i['lastName'],
+            'UserID/Email (Required)' : i['mailid'], 'Extension' : i['primaryExtension']['pattern'],
+            'Caller ID First Name' : i['firstName'], 'Caller ID Last Name' : i["lastName"],
+            'Location' : f'{location_user}', 'Hybrid Calendar Service Resource Group' : 'FALSE',
+            'Jabber with Webex Teams' : 'FALSE', 'Jabber Calling' : 'FALSE', 'UC Manager Profile' : 'FALSE',
+            'Contact Migration Required' : 'FALSE', 'Upgrade Profile' : 'FALSE', 'Calling Behavior' :'FALSE',
+            'Calling Behavior UC Manager Profile' : 'FALSE', 'Call Service Aware' : 'FALSE', 'Enterprise Content Management' : 'FALSE',
+            'Hybrid Calendar Service (Exchange)' : 'FALSE', 'Hybrid Calendar Service (Google)' : 'FALSE', 'Hybrid Message Service' : 'FALSE',
+            'Webex Meeting Assistant' : 'FALSE', f'Care Digital Channel [{user_subscription}]' : 'FALSE', f'Webex Team Meeting [{user_subscription}]' : 'FALSE',
+            f'{webex_enterprise} - WebEx Enterprise Edition [{user_subscription}]' : 'FALSE', f'Webex Calling SP Enterprise [{user_subscription}]' : 'FALSE',
+            f'Webex Teams [{user_subscription}]': 'FALSE', 'Phone Number' : b['telephoneNumber'], 'Caller ID Number' : b['telephoneNumber']
+            })
